7f833539b1f94440f5bd57df54565f2e400a7d3e
Sachin Malik <sachinmalik199821@gmail.com>
2021-03-12 14:08:01 +0530

    Added Language and DS & Algo Pages

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+
+  $(".mycard").hover(function() {
+    $(".myimg").css("background-color", "white");
+    // $(".mybtn").css("background-color","white");
+    // $(".mybtn").css("color","#4534aa");
+  });
+  //
+  // $(".mycard").mouseout(function() {
+  //   $(".myimg").css("background-color", "#d6d1f5");
+  //   $(".mybtn").css("background-color","white");
+  //   $(".mybtn").css("color","#4534aa");
+  // });
